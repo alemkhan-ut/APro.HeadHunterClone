2bc67e99055ea11102f7b8628976112b949989ca
Alemkhan <napoleon21@list.ru>
2024-05-22 21:02:04 +0500

    - fix

--- HeadHunterClone.Console/Program.cs
+++ HeadHunterClone.Console/Program.cs
@@ -1,7 +1,40 @@
-﻿internal class Program
+﻿using HeadHunterClone.Console.Vacancies;
+using SystemConsole = System.Console;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        VacancyView vacancyView = new VacancyView();
+
+        try
+        {
+            LoadData(vacancyView);
+        }
+        catch (Exception)
+        {
+            SystemConsole.Clear();
+
+            SystemConsole.WriteLine("Не удалось подключиться к серверу");
+            SystemConsole.WriteLine("Для повтороного подключения нажмите клавишу");
+            SystemConsole.ReadKey();
+
+            // TODO: Закончить повторное подключение
+        }
+
+        static void LoadData(VacancyView vacancyView)
+        {
+            while (true)
+            {
+                SystemConsole.WriteLine("Загрузка...");
+
+                SystemConsole.Clear();
+
+                vacancyView.ShowVacanciesAsync().Wait();
+
+                SystemConsole.WriteLine("Нажмите любую клавишу для загрузки");
+                SystemConsole.ReadKey();
+            }
+        }
     }
 }
--- HeadHunterClone.Console/Vacancies/VacancyView.cs
+++ HeadHunterClone.Console/Vacancies/VacancyView.cs
@@ -1,17 +1,52 @@
 ﻿using HeadHunterClone.Domain.Models;
 using Newtonsoft.Json;
+using SystemConsole = System.Console;
 
 namespace HeadHunterClone.Console.Vacancies
 {
     internal class VacancyView
     {
-        private readonly string _host = "localhost:7156/";
-        private readonly string _getVacanciesApi = "api/vacancies";
-        private readonly string _createVacancyApi = "api/vacancies/create";
-        private readonly string _deleteVacancyApi = "api/vacancies/delete";
-        private readonly string _updateVacancyApi = "api/vacancies/update";
+        private readonly string _getVacanciesApi = "https://localhost:7156/api/vacancies";
+        private readonly string _createVacancyApi = "https://localhost:7156/api/vacancies/create";
+        private readonly string _deleteVacancyApi = "https://localhost:7156/api/vacancies/delete";
+        private readonly string _updateVacancyApi = "https://localhost:7156/api/vacancies/update";
 
-        public async Task<List<Vacancy>> GetVacancies()
+
+        public async Task ShowVacanciesAsync()
+        {
+            var vacancies = await GetVacanciesAsync();
+
+            foreach (var vacancy in vacancies)
+            {
+                SystemConsole.WriteLine(vacancy.Id + " : " + vacancy.Title); // 1 : Title1
+            }
+
+            SystemConsole.Write("Выберите ID вакансии: ");
+            var input = Convert.ToInt32(SystemConsole.ReadLine());
+
+            var findVacancy = vacancies.FirstOrDefault(x => x.Id == input);
+
+            ShowVacancy(findVacancy);
+        }
+
+        public async Task ShowVacancy(Vacancy vacancy)
+        {
+            if (vacancy is null)
+            {
+                SystemConsole.WriteLine("Вакансия не найдена");
+
+                return;
+            }
+
+            SystemConsole.Clear();
+
+            SystemConsole.WriteLine("Название: " + vacancy.Title);
+            SystemConsole.WriteLine("Зарплата от " + vacancy.SalaryFrom + " до " + vacancy.SalaryTo + " " + vacancy.SalaryCurrency);
+            SystemConsole.WriteLine("Опыт работы: " + vacancy.ExperienceLevel);
+            SystemConsole.WriteLine("Описание: " + vacancy.Description);
+        }
+
+        public async Task<List<Vacancy>> GetVacanciesAsync()
         {
             using (HttpClient client = new HttpClient())
             {
@@ -22,10 +57,12 @@ namespace HeadHunterClone.Console.Vacancies
                     var responseBody = await message.Content.ReadAsStringAsync(); // получение json
 
                     var result = JsonConvert.DeserializeObject<List<Vacancy>>(responseBody); // десериализация в коллекцию вакансий
+
+                    return result;
                 }
                 else
                 {
-
+                    return null;
                 }
             }
         }
