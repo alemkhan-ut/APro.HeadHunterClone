65cb8122433470878ac8e4afd059d8be4ddb2c31
Alemkhan <napoleon21@list.ru>
2024-06-05 21:07:16 +0500

    - upadte account controller

--- HeadHunterClone.API/Controllers/AccountController.cs
+++ HeadHunterClone.API/Controllers/AccountController.cs
@@ -11,9 +11,10 @@ namespace HeadHunterClone.API.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         // localhost/api/account/register?email=alemkhja@gmail&password=23123&confirm
@@ -42,11 +43,49 @@ namespace HeadHunterClone.API.Controllers
             {
                 await _signInManager.SignInAsync(newUser, false);
 
-                return Results.Ok();
+                return Results.Ok("Пользователь успешно создан под ID: " + newUser.Id);
             }
             else
             {
-                return Results.BadRequest("Не удалось зарегистрироваться");
+                return Results.BadRequest(registerResult.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IResult> Login(string email, string password, bool rememberMe)
+        {
+            var loginResult = await _signInManager.PasswordSignInAsync(email, password, rememberMe, false);
+
+            if (loginResult.Succeeded)
+            {
+                return Results.Ok("Успешно авторизован");
+            }
+            else
+            {
+                return Results.BadRequest("Логин и/или пароль были не правильные");
+            }
+        }
+
+        [HttpPost("logout")]
+        public async Task<IResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Results.Ok("Вы успешно вышли из аккаунта");
+        }
+
+        [HttpGet("authCheck")]
+        public async Task<IResult> AuthorizationCheck()
+        {
+            string userName = _userManager.GetUserName(User);
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                return Results.NotFound("Пользователь не авторизован");
+            }
+            else
+            {
+                return Results.Ok("Вы авторизованы как " + userName);
             }
         }
 
